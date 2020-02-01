0195d257970b0e52e6332bb56d8e6e718112a968
Mayank Kesarwani <mayankkesarwani2778@gmail.com>
2020-02-01 13:48:57 +0530

    section 3 end

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 //env setup
 dotenv.config({ path: "./config/config.env" });
 //express setup
 const app = express();
+
+//logger middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//Mount router
+app.use("/api/v1/bootcamps", require("./api/v1/bootcamps"));
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
 const PORT = process.env.PORT || 5555;
 
