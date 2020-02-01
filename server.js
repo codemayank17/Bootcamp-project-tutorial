6833066ef4d840a39be44fb9bd12e4b3a9e5fa9f
Mayank Kesarwani <mayankkesarwani2778@gmail.com>
2020-02-01 15:42:45 +0530

    section 4 end

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 //env setup
 dotenv.config({ path: "./config/config.env" });
+//connect to database
+connectDB();
 //express setup
 const app = express();
+
+//Bodyparser
+app.use(express.json());
 
 //logger middleware
 if (process.env.NODE_ENV === "development") {
@@ -20,9 +26,17 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5555;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `Server started in ${process.env.NODE_ENV} enviornment and port ${PORT}`
   )
 );
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
